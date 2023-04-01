78eca3a7930b161618918d06d5c965d737919faf
algoritmocpp <xgpkey@gmail.com>
2023-04-01 11:07:18 -0300

    Completando kata-3 typescript

--- typescript/kata/kata-3/main.ts
+++ typescript/kata/kata-3/main.ts
@@ -1,8 +1,10 @@
-export function descendingOrder(n: number): any {
-  let numberString: Array<number> = n
-    .toString()
-    .split("")
-    .map((number) => parseInt(number));
-
-  return numberString;
+export function descendingOrder(n: number): number {
+  return parseInt(
+    n
+      .toString()
+      .split("")
+      .map((number) => parseInt(number))
+      .sort((a: number, b: number) => b - a)
+      .join("")
+  );
 }
