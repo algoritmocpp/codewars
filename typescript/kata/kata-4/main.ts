4cca73ac958e280cf159605d89df5e8d9d2b14eb
algoritmocpp <xgpkey@gmail.com>
2023-04-01 11:59:54 -0300

    Adicionando outras formas de se resolver, user sz-piotr

--- typescript/kata/kata-4/main.ts
+++ typescript/kata/kata-4/main.ts
@@ -34,3 +34,29 @@ export function solution(roman: string): number {
 
   return result;
 }
+
+// Other people solutions, user sz-piotr
+
+const values: Record<string, number> = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+export function solutionTwo(roman: string): number {
+  let value = 0;
+  for (let i = 0; i < roman.length; i++) {
+    const current = values[roman[i]];
+    const next = values[roman[i + 1]] || 0;
+    if (current < next) {
+      value -= current;
+    } else {
+      value += current;
+    }
+  }
+  return value;
+}
