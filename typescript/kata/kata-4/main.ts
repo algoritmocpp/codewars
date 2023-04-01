79622de5332fdcaa3a94bf5329095b87db51a642
algoritmocpp <xgpkey@gmail.com>
2023-04-01 11:24:27 -0300

    Implementando kata-4 typescript

--- typescript/kata/kata-4/main.ts
+++ typescript/kata/kata-4/main.ts
@@ -1,4 +1,23 @@
 export function solution(roman: string): number {
-  // complete the solution by transforming the
-  // string roman numeral into an integer
+  type Roman = {
+    I: number;
+    V: number;
+    X: number;
+    L: number;
+    C: number;
+    D: number;
+    M: number;
+  };
+
+  const value: Roman = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  return 0;
 }
