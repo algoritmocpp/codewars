fbe59d164573a9df10d422cda3391867f40cb10d
algoritmocpp <xgpkey@gmail.com>
2023-04-01 10:32:37 -0300

    Adicionando forma curta de resolver o problema, de winston929

--- typescript/kata/kata-1/main.ts
+++ typescript/kata/kata-1/main.ts
@@ -6,3 +6,11 @@ export function countSheeps(arrayOfSheep: (boolean | undefined | null)[]) {
 
   return sheepCount;
 }
+
+// Solutions of other people, more clever and best pratices
+
+export function countSheepsClever(
+  arrayOfSheep: (boolean | undefined | null)[]
+) {
+  return arrayOfSheep.filter(Boolean).length;
+}
